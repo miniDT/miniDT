0cc57e81246a94673ead444da08c4ce239bf450a
Nazar Bartosik <nazar.bartosik@cern.ch>
2019-07-31 13:09:15 +0200

    Improved alignment of the chambers in config ile

--- modules/reco/config.py
+++ modules/reco/config.py
@@ -5,23 +5,23 @@ from math import pi
 
 sl_w = 700     # chamber width (along X)
 sl_l = 700     # chamber length (along Y)
-sl_h = 72       # chamber height
-dsl = 500       # distance between chambers 0 and 2
+sl_h = 62       # chamber height
+dsl = 782       # Z of chamber 2
 
 # Shifts of each SL along the (X,Y,Z) axis
 SL_SHIFT = {
-    0: (0.5*sl_w,    0.5*sl_l,    0.5*sl_h),
-    1: (0.5*sl_l,    0.5*sl_w,    1.5*sl_h),
-    2: (0.5*sl_w,    0.5*sl_l,    1.5*sl_h + dsl),
-    3: (0.5*sl_l,    0.5*sl_w,    2.5*sl_h + dsl)
+    0: (0.5*sl_w +10,   0.5*sl_l,      0.5*sl_h),
+    1: (0.5*sl_l,      0.5*sl_w +10,  1.5*sl_h),
+    2: (0.5*sl_w,      0.5*sl_l,      0.5*sl_h + dsl),
+    3: (0.5*sl_l,      0.5*sl_w,      1.5*sl_h + dsl)
 }
 
 # Rotation of each SL around the (X,Y,Z) axis
 SL_ROTATION = {
-    0: (0,      0,        0),
-    1: (0,      0,   0.5*pi),
-    2: (0,      0,        0),
-    3: (0,      0,   0.5*pi)
+    0: (0,        0,          0),
+    1: (0,        0,     0.5*pi),
+    2: (0,        0,          0),
+    3: (0,        0,     0.5*pi)
 }
 
 # Precisions
@@ -29,7 +29,7 @@ DTYPE_COOR = np.float64
 FIT_CHI2_MAX = 2.0
 
 # Colors of fitted tracks
-TRACK_COLORS = ['limegreen', 'darkolivegreen', 'goldenrod', 'peru', 'tomato', 'maroon']
+TRACK_COLORS = ['limegreen', 'forestgreen', 'goldenrod', 'peru', 'tomato', 'maroon']
 
 # Fit parameters
 NHITS_MIN_LOCAL = 3
